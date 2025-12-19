c79c13f75e5cf5175cb8cf4df6d05bfad57beea3
Yossi-Spindler_verint <yossi.spindler@verint.com>
2025-12-19 20:16:45 +0200

    l3-mgw

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 from flask import Flask, send_from_directory
-import os
 
-app = Flask(__name__, static_folder='frontend_build', static_url_path='')
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
-def serve_index():
-    return send_from_directory(app.static_folder, 'index.html')
-
-@app.route('/<path:path>')
-def serve_static(path):
-    file_path = os.path.join(app.static_folder, path)
-    if os.path.exists(file_path):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
+def serve_quiz():
+    return send_from_directory(app.static_folder, 'quiz.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
